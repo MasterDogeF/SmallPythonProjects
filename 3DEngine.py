ff8a2435a43fd9b925a908373f319921045d1b96
MasterDogeF <doge.tsokur@gmail.com>
2026-01-27 22:49:44 +0100

    Update 3DEngine.py

--- 3DEngine.py
+++ 3DEngine.py
@@ -1,10 +1,11 @@
 import pygame
 import time
 import math
+import random
 
 background_colour = (10,10,10)
 foreground_colour = (0,255,0)
-(width, height) = (800, 800)
+(width, height) = (1200, 1200)
 FPS = 60
 
 display = pygame.display.set_mode((width, height))
@@ -23,6 +24,9 @@ def point(point):
 
 def line(p1, p2):
     pygame.draw.line(display, foreground_colour, p1, p2)
+
+def rect(p1, p2, p3, p4):
+    pygame.draw.polygon(display, (255,0,0), (p1,p2,p3,p4))
 
 def screen(point): #convert coordinate system from where 0 is centre to where 0 is top left
     x = point[0]
@@ -51,6 +55,14 @@ def rotate_xz(point, angle):
 
     return ((x * math.cos(angle) - z * math.sin(angle), y, x * math.sin(angle) + z * math.cos(angle))) #rotate vector forumula
 
+def rotate_yz(point, angle):
+    x = point[0]
+    y = point[1]
+    z = point[2]
+
+    return ((x, y * math.cos(angle) - z * math.sin(angle), y * math.sin(angle) + z * math.cos(angle))) #rotate vector forumula
+
+
 vertices = [
     (0.25, 0.25, 0.25),
     (-0.25, 0.25, 0.25),
@@ -63,7 +75,7 @@ vertices = [
     (0.25, -0.25, -0.25),
 ]
 
-faces = [
+connections = [
     [0, 1, 2, 3],
     [4, 5, 6, 7],
     [0, 4],
@@ -72,26 +84,50 @@ faces = [
     [3, 7],
 ]
 
+faces = [
+    [0, 1, 2, 3],
+    [4, 5, 6, 7],
+    [6, 5, 1, 2],
+    [5, 4, 0, 1],
+    [7, 6, 2, 3],
+]
+
 
 dz = 1
 dx = 0.0
 dy = 0.0
-angle = 0
+horizontal_angle = 0
+vertical_angle = 0
 
 def frame():
     clear()
     #for vertex in vertices:
-        #point(screen(project(translate(rotate_xz(vertex, angle), dx, dy, dz))))
+        #point(screen(project(translate(rotate_yz(rotate_xz(vertex, horizontal_angle), vertical_angle), dx, dy, dz))))
+
     for face in faces:
+        a = vertices[face[0]]
+        b = vertices[face[1]]
+        c = vertices[face[2]]
+        d = vertices[face[3]]
+
+        rect(
+            screen(project(translate(rotate_yz(rotate_xz(a, horizontal_angle), vertical_angle), dx, dy, dz))),
+            screen(project(translate(rotate_yz(rotate_xz(b, horizontal_angle), vertical_angle), dx, dy, dz))),
+            screen(project(translate(rotate_yz(rotate_xz(c, horizontal_angle), vertical_angle), dx, dy, dz))),
+            screen(project(translate(rotate_yz(rotate_xz(d, horizontal_angle), vertical_angle), dx, dy, dz)))
+        )
+
+    for connection in connections:
         #connecting each contiguous vertex with a line 
-        for i in range(0, len(face)):
-            a = vertices[face[i]] #first vertex
-            b = vertices[face[(i+1)%len(face)]] # wrap around
+        for i in range(0, len(connection)):
+            a = vertices[connection[i]] #first vertex
+            b = vertices[connection[(i+1)%len(connection)]] # last vertex + wrap around
 
             line(
-                screen(project(translate(rotate_xz(a, angle), dx, dy, dz))),
-                screen(project(translate(rotate_xz(b, angle), dx, dy, dz)))
+                screen(project(translate(rotate_yz(rotate_xz(a, horizontal_angle), vertical_angle), dx, dy, dz))),
+                screen(project(translate(rotate_yz(rotate_xz(b, horizontal_angle), vertical_angle), dx, dy, dz)))
             )
+
 
     pygame.display.flip()
 
@@ -100,7 +136,8 @@ while running:
     delta = 1/FPS
     time.sleep(delta)
 
-    angle += math.pi*delta
+    horizontal_angle += math.pi*delta
+    #vertical_angle += math.pi*delta / 2
     frame()
 
     keys = pygame.key.get_pressed()
