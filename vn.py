83b672fe89c6c388f4c1e91350dde0832eb28bf0
MasterDogeF <doge.tsokur@gmail.com>
2026-02-15 15:55:36 +0100

    vn update

--- vn.py
+++ vn.py
@@ -1,6 +1,7 @@
 import pygame
 import random
 import json
+import asyncio
 
 background_colour = (10,10,10)
 (width, height) = (1280, 720)
@@ -18,10 +19,8 @@ nameTxt = font.render('', True, 'white')
 count = 0
 textRect = dialogueTxt.get_rect()
 nameRect = nameTxt.get_rect()
-textRect.center = (0 + (width // 3), height - (height // 8))
-nameRect.center = (0 + (width // 3), height - (height // 5.5))
-
-dialogue_bg = pygame.Surface((width, height), pygame.SRCALPHA)
+textRect.center = (0 + (width // 3), height - (height // 5.5))
+nameRect.center = (0 + (width // 3), height - (height // 4.5))
 
 def getScenes():
     with open("Scenes.json") as file:
@@ -40,7 +39,7 @@ currentScene = Scenes["intro"]
 textIndex = 0
 awaitingInput = False
 charbodys = []
-
+type_timer = 0.0
 
 running = True
 while running:
@@ -51,8 +50,10 @@ while running:
     bg = currentScene["bg"]
 
     text = currentTexts[textIndex]
-
-    timer.tick(60)
+    
+    dt = timer.tick(60)
+    ms_per_char = 50 / speed
+    print(dt)
 
     #bg
     img = pygame.image.load(bg+".png").convert()
@@ -62,34 +63,57 @@ while running:
     if text[0] == "char+": 
         charbodys.append(text[1])
         textIndex += 1
-        text = currentTexts[textIndex]
+        count = 0
+        type_timer = 0.0
+        continue
     elif text[0] == "char-":
-        charbodys.remove(text[1])
+        for char in charbodys:
+            if char["name"] == text[1]:
+                char["fading"] = "out"
         textIndex += 1
-        text = currentTexts[textIndex]
-
-    for char in charbodys: #character bodys
-        img = pygame.image.load(char[0].lower()+"full.png").convert_alpha()
+        count = 0
+        type_timer = 0.0
+        continue
+    
+    for char in charbodys:
+        if char["fading"].lower() == "in":
+            if char["alpha"] >= 255:
+                char["fading"] == "none"
+            else:
+                char["alpha"] += dt / 1.25
+        elif char["fading"].lower() == "out":
+            if char["alpha"] <= 0:
+                char["fading"] == "none"
+                charbodys.remove(char)
+            else:
+                char["alpha"] -= dt / 1.25
+    
+        img = pygame.image.load(char["name"].lower()+"full.png").convert_alpha()
         img = pygame.transform.scale(img, ((width / 3.88)*1.75, (height / 1.3)*1.75))
         x, y = 0, height // 16
-        match char[1]:
+        match char["pos"]:
             case "centre":
                 rect = img.get_rect(midtop=(width // 2, y))
+        img.set_alpha(char["alpha"])
         display.blit(img, rect)
-    
-    pygame.draw.rect(dialogue_bg, (69, 69, 69, 120), (width//3.5, 0, width, height//3.5)) #dialogue box
-    display.blit(dialogue_bg, (0, height-(height // 3.5)))
+
+    dialoguebox = pygame.image.load("dialoguebox.png").convert_alpha()
+    dialoguebox = pygame.transform.scale(dialoguebox, (width//1.4, height//3.5))
+    dialoguebox.set_alpha(169)
+    display.blit(dialoguebox, (width-(width//1.4), height-(height // 3.5)))
+
+    #pygame.draw.rect(dialogue_bg, (69, 69, 69, 120), (width//3.5, 0, width, height//3.5)) #dialogue box
+    #display.blit(dialogue_bg, (0, height-(height // 3.5)))
 
     dialogueTxt = font.render(text[1][0:count], True, 'white')
     nameTxt = font.render(text[0], True, 'white')
 
-    multi_line_text(text[1][0:count])
     display.blit(nameTxt, nameRect)
 
     if text[0] != "": #character head
         charfound = False
         for char in charbodys:
-            if char[0] == text[0].lower():
+            if char["name"] == text[0].lower():
                 charfound = True
                 
         if not charfound:
@@ -99,10 +123,14 @@ while running:
         
     if count < len(text[1]):
         awaitingInput = False
-        count += 1
+        type_timer += dt
+        while type_timer >= ms_per_char and count < len(text[1]):
+            count += 1
+            type_timer -= ms_per_char
     else:
         awaitingInput = True
 
+    multi_line_text(text[1][0:count])
 
     pygame.display.flip()
 
@@ -112,10 +140,14 @@ while running:
         if event.type == pygame.QUIT:
             running = False
             
-        elif event.type == pygame.MOUSEBUTTONDOWN and awaitingInput:
-            count = 0
-            textIndex += 1
-            if textIndex >= len(currentTexts):
-                currentScene = Scenes[currentScene["next"]]
-                charbodys = []
-                textIndex = 0
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            if awaitingInput:
+                count = 0
+                textIndex += 1
+                type_timer = 0.0
+                if textIndex >= len(currentTexts):
+                    currentScene = Scenes[currentScene["next"]]
+                    charbodys = []
+                    textIndex = 0
+            else:
+                count = len(text[1])
